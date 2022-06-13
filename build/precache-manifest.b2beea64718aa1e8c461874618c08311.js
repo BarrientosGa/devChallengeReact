2ade7ca7ef79efbf61211b72dfdf6a3f3d634798
Gabriel Barrientos <gabrielbarrientos729@gmail.com>
2022-06-13 14:51:44 -0300

    build

---
+++ build/precache-manifest.b2beea64718aa1e8c461874618c08311.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6830734b6ec14befdf3b",
+    "url": "/static/css/main.7a41c814.chunk.css"
+  },
+  {
+    "revision": "6830734b6ec14befdf3b",
+    "url": "/static/js/main.6d1ff9ee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1de3a44f88a9390497af",
+    "url": "/static/css/2.f3ba98c6.chunk.css"
+  },
+  {
+    "revision": "1de3a44f88a9390497af",
+    "url": "/static/js/2.2d7bfb4b.chunk.js"
+  },
+  {
+    "revision": "4d98555d479502e80d7364b8d3e98099",
+    "url": "/index.html"
+  }
+];
